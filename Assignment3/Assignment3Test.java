f51ea0974f965bfb8e7344d66c3a8491d81f5163
Kevin Leary <kevin.n.leary@gmail.com>
2023-09-26 20:05:30 -0400

    added blank test file

---
+++ Assignment3/Assignment3Test.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.*;
+
+class Assignment3Test {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+    @Test
+    void calculateMean() {
+
+    }
+
+    @Test
+    void calculateMedian() {
+
+    }
+
+    @Test
+    void calculateMode() {
+
+    }
+
+    @Test
+    void calculateStandardDeviation() {
+
+    }
+}
