d4311ddfea52c48cff93e47e06505657dfde8a01
Kevin Leary <kevin.n.leary@gmail.com>
2023-09-26 21:54:29 -0400

    updated Test file a little

--- Assignment3/Assignment3Test.java
+++ Assignment3/Assignment3Test.java
@@ -1,9 +1,16 @@
 import org.junit.jupiter.api.*;
 
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class Assignment3Test {
 
     @BeforeEach
     void setUp() {
+        double[] numbers1 = {1,2,3,4,5,6};
+        double[] numbers2 = {-100000,0,100000};
+        double[] numbers3 = {1,2,2,1,3,3};
 
     }
 
@@ -13,22 +20,33 @@ class Assignment3Test {
     }
 
     @Test
-    void calculateMean() {
+    void testCalculateMean() {
+        double[] numbers = {1, 1, 2, 3, 4, 4, 5, 5};
+//        double[] numbers = {1,2,2,1,3,3};
 
+        Assignment3.calculateMean(numbers);
+        assertEquals(3.125, Assignment3.calculateMean(numbers));
     }
 
     @Test
-    void calculateMedian() {
+    void testCalculateMedian() {
+        double[] numbers = {1, 1, 2, 3, 4, 4, 5, 5};
+//        double[] numbers = {-1000000, 0, 1000000};
 
+        Assignment3.calculateMedian(numbers);
+        assertEquals(3.5 , Assignment3.calculateMedian(numbers));
     }
 
     @Test
-    void calculateMode() {
+    void testCalculateMode() {
+        double[] numbers = {-1000000, 0, 1000000};
 
+        Assignment3.calculateMode(numbers);
+        assertEquals(-1000000, 0, 1000000 , Assignment3.calculateMode(numbers));
     }
 
     @Test
-    void calculateStandardDeviation() {
+    void testCalculateStandardDeviation() {
 
     }
 }
